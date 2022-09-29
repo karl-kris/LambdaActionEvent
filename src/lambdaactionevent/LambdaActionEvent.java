a127837c4942e53f7b5d13ae78040e95f33e1063
Kristian Johansson <carlkristian.johansson@gmail.com>
2022-09-29 13:20:11 +0200

    ActionEvent usando expresiones Lambda

--- src/lambdaactionevent/LambdaActionEvent.java
+++ src/lambdaactionevent/LambdaActionEvent.java
@@ -35,26 +35,31 @@ public class LambdaActionEvent extends Application {
         Label interestRate = new Label("Annual interest rate: ");
         root.add(interestRate, 0, 1);        
         TextField interestTextField = new TextField();
+        interestTextField.setAlignment(Pos.CENTER_RIGHT);
         root.add(interestTextField, 1, 1);
         
         Label numYears = new Label("Number of Years: ");
         root.add(numYears, 0, 2);        
         TextField yearsTextField = new TextField();
+        yearsTextField.setAlignment(Pos.CENTER_RIGHT);
         root.add(yearsTextField, 1, 2);
         
         Label loanAmount = new Label("Loan Amount: ");
         root.add(loanAmount, 0, 3);       
         TextField loanTextField = new TextField();
+        loanTextField.setAlignment(Pos.CENTER_RIGHT);
         root.add(loanTextField, 1, 3);
         
         Label monthlyPayment = new Label("Monthly payment: ");
         root.add(monthlyPayment, 0, 4);       
         TextField payTextField = new TextField();
+        payTextField.setAlignment(Pos.CENTER_RIGHT);
         root.add(payTextField, 1, 4);
         
         Label total = new Label("Total payment: ");
         root.add(total, 0, 5);       
         TextField totalTextField = new TextField();
+        totalTextField.setAlignment(Pos.CENTER_RIGHT);
         root.add(totalTextField, 1, 5);
         
         Button btn = new Button("Calculate");
@@ -72,8 +77,8 @@ public class LambdaActionEvent extends Application {
             double denominator = 1 - Math.pow((1+MonthInterest),(-12 * years));
             double MonthPay = (LoanAmount * MonthInterest)/denominator;
             double TotalPay = MonthPay * (years*12);
-            payTextField.setText(String.format("%.2f", MonthPay));
-            totalTextField.setText(String.format("%.2f", TotalPay));
+            payTextField.setText(String.format("$%.2f", MonthPay));
+            totalTextField.setText(String.format("$%.2f", TotalPay));
         });
         
         Scene scene = new Scene(root, 400, 250);
